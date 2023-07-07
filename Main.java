bfb56e779fcc6a84c6ffceebced56c8d7f94f404
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-07 09:46:23 -0300

    Piece and Board classes

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
-import entities.boardgame.Position;
+import entities.boardgame.Board;
 
 public class Main {
 
     public static void main(String[] args) {
         
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
+
+               
 
     }
 
