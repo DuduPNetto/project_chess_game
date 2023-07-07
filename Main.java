1e430b26d651dee53a44bb46ee6c14322324dd45
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-07 12:23:30 -0300

    Creating ChessException

--- Main.java
+++ Main.java
@@ -3,10 +3,12 @@ import entities.chess.ChessMatch;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
-
+        
+        
+        
     }
 
 }
