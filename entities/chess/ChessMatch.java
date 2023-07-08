4463f624d433bd1fcc6adc5519496b03c347926a
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-07 22:24:29 -0300

    Bishop piece

--- entities/chess/ChessMatch.java
+++ entities/chess/ChessMatch.java
@@ -9,6 +9,7 @@ import entities.board.Piece;
 import entities.board.Position;
 import entities.chess.enums.Color;
 import entities.chess.exceptions.ChessException;
+import entities.chess.pieces.Bishop;
 import entities.chess.pieces.King;
 import entities.chess.pieces.Pawn;
 import entities.chess.pieces.Rook;
@@ -201,8 +202,10 @@ public class ChessMatch {
 
 	private void initialSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
-		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
@@ -213,8 +216,10 @@ public class ChessMatch {
 		placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 		
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
